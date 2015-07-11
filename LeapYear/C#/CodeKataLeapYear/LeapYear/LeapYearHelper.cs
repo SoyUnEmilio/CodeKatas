101e36259114bd5ff54e6a1e3c15e6d5469e3c80
Emilio López Arias <emilio.lopez@neoris.com>
2015-07-11 17:23:46 +0200

    Added solution Added TestingNormalLeapYear Red State

---
+++ LeapYear/C#/CodeKataLeapYear/LeapYear/LeapYearHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    public class LeapYearHelper
+    {
+        public static bool IsLeapYear(int year)
+        {
+            return false;
+        }
+    }
+}
---
+++ LeapYear/C#/CodeKataLeapYear/LeapYearTests/LeapYearHelperTest.cs
@@ -0,0 +1,17 @@
+﻿using LeapYear;
+using NUnit.Framework;
+
+namespace LeapYearTests
+{
+    [TestFixture]
+    public class LeapYearHelperTest
+    {
+        [Test]
+        public void TestingNormalLeapYear()
+        {
+            Assert.IsTrue(LeapYearHelper.IsLeapYear(1996));
+        }
+
+
+    }
+}
